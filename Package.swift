c1cc450b00ade752b9e5112ff0a15cc4d1042fc6
Ryan Schildknecht <schildknecht618@gmail.com>
2025-06-25 22:09:20 -0500

    Minimum Information
    - Figure at tests at a later time.

--- Package.swift
+++ Package.swift
@@ -6,19 +6,12 @@ import PackageDescription
 let package = Package(
     name: "SwiftDataService",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftDataService",
             targets: ["SwiftDataService"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftDataService"),
-        .testTarget(
-            name: "SwiftDataServiceTests",
-            dependencies: ["SwiftDataService"]
-        ),
     ]
 )
--- Sources/SwiftDataService/SwiftDataService.swift
+++ Sources/SwiftDataService/SwiftDataService.swift
@@ -1,2 +1,69 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import SwiftData
+
+@available(macOS 14, iOS 17, tvOS 17, watchOS 10, *)
+protocol SwiftDataServiceProtocol<T> {
+    associatedtype T: PersistentModel
+    func fetchData(predicate: Predicate<T>?, sortBy: [SortDescriptor<T>]) -> [T]
+    func addData(_ notepad: T)
+    func removeData(_ notepad: T)
+}
+
+// Default Arguements for Protocol
+@available(macOS 14, iOS 17, tvOS 17, watchOS 10, *)
+extension SwiftDataServiceProtocol {
+    func fetchData(predicate: Predicate<T>? = nil,
+                       sortBy: [SortDescriptor<T>] = .init()) -> [T] {
+        self.fetchData(predicate: predicate, sortBy: sortBy)
+    }
+}
+
+@available(macOS 14, iOS 17, tvOS 17, watchOS 10, *)
+open class SwiftDataService<T: PersistentModel>: SwiftDataServiceProtocol {
+    
+    private let modelContainer: ModelContainer
+    private let modelContext: ModelContext
+    
+    init(modelContainer: ModelContainer, modelContext: ModelContext) {
+        self.modelContext = modelContext
+        self.modelContainer = modelContainer
+    }
+    
+    @MainActor
+    required public init() {
+        let schema = Schema([
+            T.self,
+        ])
+        self.modelContainer = try! ModelContainer(for: schema,
+                                                  configurations: ModelConfiguration(isStoredInMemoryOnly: false))
+        self.modelContext = modelContainer.mainContext
+    }
+    
+    func fetchData(predicate: Predicate<T>? = nil,
+                   sortBy: [SortDescriptor<T>] = .init()) -> [T] {
+        do {
+            return try modelContext.fetch(FetchDescriptor<T>(predicate: predicate,
+                                                             sortBy: sortBy))
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    func addData(_ item: T) {
+        modelContext.insert(item)
+        saveContext()
+    }
+    
+    func removeData(_ item: T) {
+        modelContext.delete(item)
+        saveContext()
+    }
+    
+    private func saveContext() {
+        do {
+            try modelContext.save()
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+}
